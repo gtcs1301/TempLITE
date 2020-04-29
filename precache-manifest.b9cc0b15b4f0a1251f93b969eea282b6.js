dccf84fb71669e2fae57d2a712d4a9a13fab4513
Anthony Zheng <anthonyzheng0290@gmail.com>
2020-04-29 17:36:29 -0400

    v1 build

---
+++ precache-manifest.b9cc0b15b4f0a1251f93b969eea282b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0633a6faea31c274dc92d12679494ead",
+    "url": "/TempLITE/index.html"
+  },
+  {
+    "revision": "5aff5663e4997c5715b9",
+    "url": "/TempLITE/static/css/2.5cb0ef23.chunk.css"
+  },
+  {
+    "revision": "bdcd339eaac6846911a2",
+    "url": "/TempLITE/static/css/main.15205e79.chunk.css"
+  },
+  {
+    "revision": "5aff5663e4997c5715b9",
+    "url": "/TempLITE/static/js/2.8fa01f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TempLITE/static/js/2.8fa01f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdcd339eaac6846911a2",
+    "url": "/TempLITE/static/js/main.b3c941e6.chunk.js"
+  },
+  {
+    "revision": "f5f8af6a204209f039f9",
+    "url": "/TempLITE/static/js/runtime-main.287b7346.js"
+  },
+  {
+    "revision": "f38dce0a923f80b613ee1c7450c7a807",
+    "url": "/TempLITE/static/media/Regular.f38dce0a.otf"
+  },
+  {
+    "revision": "fa9fdf3624cdb506adadb743d34dedb3",
+    "url": "/TempLITE/static/media/RegularItalic.fa9fdf36.otf"
+  },
+  {
+    "revision": "9b1cbed4f1e6a2ef9822e944e0332b26",
+    "url": "/TempLITE/static/media/Semibold.9b1cbed4.otf"
+  }
+]);
